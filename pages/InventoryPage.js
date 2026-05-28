7aac14cc188386b777bda9f9613b31d525eb81f1
Sweety Darji <sweety.darji@project-kiwiqa.com>
2026-05-28 14:59:49 +0530

    Fix and stabilize Playwright test suite

--- pages/InventoryPage.js
+++ pages/InventoryPage.js
@@ -1,12 +1,26 @@
-export default class InventoryPage {
+import { expect } from '@playwright/test';
+
+export class InventoryPage {
   constructor(page) {
     this.page = page;
 
     this.title = page.locator('.title');
 
-    this.backpackAddBtn = page.locator('#add-to-cart-sauce-labs-backpack');
+    this.backpackAddBtn = page.locator(
+      '#add-to-cart-sauce-labs-backpack'
+    );
 
-    this.sortDropdown = page.locator('[data-test="product-sort-container"]');
+    this.bikeLightAddBtn = page.locator(
+      '#add-to-cart-sauce-labs-bike-light'
+    );
+
+    this.sortDropdown = page.locator(
+      '[data-test="product-sort-container"]'
+    );
+
+    this.cartBadge = page.locator('.shopping_cart_badge');
+
+    this.inventoryItems = page.locator('.inventory_item');
   }
 
   async verifyInventoryPageLoaded() {
@@ -19,5 +33,22 @@ export default class InventoryPage {
 
   async addFirstProductToCart() {
     await this.backpackAddBtn.click();
+  }
+
+  async addMultipleProducts() {
+    await this.backpackAddBtn.click();
+    await this.bikeLightAddBtn.click();
+  }
+
+  async verifyCartCount(count) {
+    await expect(this.cartBadge).toHaveText(count.toString());
+  }
+
+  async sortProducts(value) {
+    await this.sortDropdown.selectOption(value);
+  }
+
+  async verifyProductsVisible() {
+    await expect(this.inventoryItems.first()).toBeVisible();
   }
 }
